292509890011a7c10dadef59215f6c592800c97c
Ashwin Elangovan <ashelangovan@gmail.com>
2023-11-19 15:42:53 -0700

    Added sleep computation
    Signed-off-by: Ashwin Elangovan <ashelangovan@gmail.com>

--- constants.py
+++ constants.py
@@ -3,6 +3,6 @@ REGISTRATION_REQUIRED_FIELDS = ['name', 'email', 'phone', 'allergies', 'emergenc
 
 USER_ATTRIBUTE_REQUIRED_FIELDS = ['heart_rate', 'respiratory_rate', 'steps_count', 'calories_burnt', 'blood_oxygen', 'sleep', 'timestamp']
 
-USER_ATTRIBUTE_FETCH_KEYS = {'heart_rate', 'respiratory_rate', 'steps_count', 'calories_burnt', 'blood_oxygen'}
+USER_ATTRIBUTE_FETCH_KEYS = {'heart_rate', 'respiratory_rate', 'steps_count', 'calories_burnt', 'blood_oxygen', 'sleep'}
 
 WEATHER_API_HOST = 'https://api.openweathermap.org/data/2.5/weather'
