f8ab4715e8c935371d88db73f808ee32dd0263aa
Ashwin Elangovan <ashelangovan@gmail.com>
2023-11-26 12:46:25 -0700

    Push jobs base framework
    Signed-off-by: Ashwin Elangovan <ashelangovan@gmail.com>

--- dataAccess/mongoData.py
+++ dataAccess/mongoData.py
@@ -2,6 +2,7 @@ from bson.objectid import ObjectId
 from flask_pymongo import PyMongo
 import os
 from dotenv import load_dotenv
+from pymongo.mongo_client import MongoClient
 
 load_dotenv()
 
@@ -14,11 +15,16 @@ class mongoData:
     def __get_mongo(self):
         if not self.app.config['TESTING']:
             self.app.config['MONGO_URI'] = os.getenv('DB_URI')
+            # Local DB
             # self.app.config['MONGO_URI'] = 'mongodb://127.0.0.1:27017/GuardianAngel?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.0.2'
         else:
-            self.app.config['MONGO_URI'] = 'mongodb://localhost:27017/testdb?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.0.2'
+            self.app.config['MONGO_URI'] = os.getenv('TEST_DB_URI')
 
         return PyMongo(self.app)
+
+    def mongo_client(self):
+        return MongoClient(self.app.config['MONGO_URI'])
+
 
     def get_all(self):
         return self.__get_mongo().db.dataSources.find({})
